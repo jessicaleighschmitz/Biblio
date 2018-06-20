ebf904d677a555a6ce0e85f21507493c465811a3
Student <example@example.com>
2018-06-20 10:54:30 -0700

    fix header background image styles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // import $ from 'jquery';
+import './styles.scss';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
@@ -18,27 +19,34 @@ module.exports = {
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin({filename:'bundle.css'}),
     new HtmlWebpackPlugin({
       title: 'JS Template',
       template: './src/index.html',
+      filename: 'index.html',
       inject: 'body',
     }),
-    new CopyWebpackPlugin([
-    { from: 'src/home.html', to: ''},
-    { from: 'src/lineup.html', to: ''},
-    { from: 'src/book-info.html', to: ''},
-    { from: 'src/bookshelf.html', to: ''},
-    { from: 'src/search.html', to: ''}
-  ])
+    new HtmlWebpackPlugin({
+      template: './src/home.html',
+      filename: 'home.html',
+      inject: 'body',
+    }),
+  //   new CopyWebpackPlugin([
+  //   { from: 'src/home.html', to: ''},
+  //   { from: 'src/lineup.html', to: ''},
+  //   { from: 'src/book-info.html', to: ''},
+  //   { from: 'src/bookshelf.html', to: ''},
+  //   { from: 'src/search.html', to: ''}
+  // ])
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        })
       },
       {
         test: /\.js$/,
